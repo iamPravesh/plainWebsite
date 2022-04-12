f20dfa90dbf346ac2f9edb007b3407791595c1a4
Pravesh Niroula <Pravesh31.745421@pmc.edu.np>
2022-04-12 09:36:19 +0545

    final

--- project/Home/script.js
+++ project/Home/script.js
@@ -1,4 +1,16 @@
 
+
 function test() {
     document.getElementById('play').innerHTML = 'this is a test';
+}
+
+function subscripted() {
+    var email = document.getElementById('email').value;
+    var email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if (email_regex.test(email)) {
+        alert('You have subscribed to our newsletter');
+    } else {
+        alert('Please enter a valid email address');
+    }
+    document.getElementById('email').value = '';
 }
